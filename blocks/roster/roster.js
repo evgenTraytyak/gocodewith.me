e030dad3d4904eb85f1a58a0c027cc1b70db3cdc
Sergey <sergeyshchurko1987@gmail.com>
2014-10-18 16:19:50 +0300

    Add roster render item

--- blocks/roster/roster.js
+++ blocks/roster/roster.js
@@ -1,4 +1,5 @@
 function Roster() {
+  var _this = this;
 
   this.users = [
     {
@@ -11,11 +12,21 @@ function Roster() {
       name: 'John'
     }
   ]
+
+  this.usersListEl = $('.roster-list');
+  this.userTpl = _.template('<li class="roster-item"><%= name %></li>');
+
+  _.forEach(this.users, function(user) {
+    _this.add(user);
+  });
 }
 
-Roster.prototype.add = function() {
-
+Roster.prototype.add = function(user) {
+  this.usersListEl.append(this.userTpl(user));
 }
+
+new Roster;
+
 
 
 
---
+++ index.js
@@ -0,0 +1,34 @@
+
+
+function Roster() {
+  var _this = this;
+
+  this.users = [
+    {
+      name: 'Nike'
+    }
+    ,{
+      name: 'Max'
+    }
+    ,{
+      name: 'John'
+    }
+  ]
+
+  this.usersListEl = $('.roster-list');
+  this.userTpl = _.template('<li class="roster-item"><%= name %></li>');
+
+  _.forEach(this.users, function(user) {
+    _this.add(user);
+  });
+}
+
+Roster.prototype.add = function(user) {
+  this.usersListEl.append(this.userTpl(user));
+}
+
+new Roster;
+
+
+
+
