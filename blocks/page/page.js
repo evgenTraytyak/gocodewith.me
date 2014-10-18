71ef34fe40f2af30be161dab2b4cc6c355d70f61
dvovna <d.vovna.dv@gmail.com>
2014-10-18 17:01:10 +0300

    Client side impl
    Roster class impl;
    template moved to html file;
    global name space introduced;
    globals section for jshint updated;

--- blocks/page/page.js
+++ blocks/page/page.js
@@ -1,2 +1,23 @@
+var Team1 = Team1 || {};
 
+Team1 = {
+  stubUsers: [
+    {
+      name: "Nike"
+    }
+    ,{
+      name: "Max"
+    }
+    ,{
+      name: "John"
+    }
+  ]
 
+  , start : function () {
+    this.Roster = new Team1.Roster(this.stubUsers)
+  }
+}
+
+$(document).ready(function () {
+  Team1.start()
+})
