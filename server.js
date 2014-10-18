16c4ecd1f8b658a2dd0e8dffd94f69a42e2d0f22
Sergey Davydovsky <electron931@gmail.com>
2014-10-18 12:49:52 +0200

    Read index file

--- server.js
+++ server.js
@@ -3,11 +3,24 @@ var http = require('http')
   , _ = require('lodash-node')
 
   //app variables
-  , PORT = 3000;
+  , PORT = 80 // server http port
+  , indexFile = 'index.html'
 
 
-var server = http.createServer(function(request, response) {
-  
-}).listen(PORT);
+http.createServer(function(request, response) {
+  //reading index file
+  fs.readFile(indexFile, function(err, page) {
+    if (err) {
+      console.log('error reading file')
+      response.writeHeader(500)
+      response.end('error reading file')
+      return;
+    }
+    
+    response.writeHeader(200, {"Content-Type": "text/html"})
+    response.write(page)
+    response.end()
+  });
+}).listen(PORT)
 
-console.log('Server started at port ' + PORT);
+console.log('Server started at port ' + PORT)
