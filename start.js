e7afec6ee6e9719fddfd14fea19ac01530eaf330
Sergey <sergeyshchurko1987@gmail.com>
2014-11-15 13:02:00 +0300

    Create start.js

---
+++ start.js
@@ -0,0 +1,19 @@
+var exec = require('child_process').exec
+
+console.log('Installing node_modules ...')
+exec('npm install', function() {
+
+  console.log('Node_modules was installed')
+  console.log('Installing bower components ...')
+  exec('bower install', function() {
+
+    console.log('Bower components were installed ...')
+    console.log('Run gulp ...')
+    exec('npm make run', function() {
+
+      console.log('Gulp was made')
+      console.log('Started server')
+      exec('node server.js');
+    });
+  })
+})
