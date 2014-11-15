d062f76b6251e267fd069a11e1d7c38bab02fb77
Sergey <sergeyshchurko1987@gmail.com>
2014-11-15 14:44:12 +0300

    Add error handlers and bug fixes start.js

--- start.js
+++ start.js
@@ -1,23 +1,25 @@
 var exec = require('child_process').exec
 
 console.log('Installing node_modules ...')
-exec('npm install', function() {
+exec('npm install', function(err) {
+
+  if (err !== null) console.error('Exec error: \'npm install\'' + err)
 
   console.log('Node_modules was installed')
   console.log('Installing bower components ...')
-  exec('bower install', function() {
+  exec('bower install', function(err) {
+
+    if (err !== null) console.error('Exec error \' Bower install\': ' + err)
 
     console.log('Bower components were installed ...')
     console.log('Run gulp ...')
-<<<<<<< HEAD
-    exec('npm make run', function() {
-=======
-    exec('gulp', function() {
->>>>>>> origin/deploy
+    exec('npm run make', function(err) {
+
+      if (err !== null) console.error('Exec error \'Gulp\': ' + err)
 
       console.log('Gulp was made')
       console.log('Started server')
-      exec('node server.js');
+      exec('node server.js')
     });
   })
 })
