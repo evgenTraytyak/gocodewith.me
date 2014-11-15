72bb41e6d871c9e04bbc87c4c01c62ab00fc43f6
dvovna <d.vovna.dv@gmail.com>
2014-11-15 16:56:04 +0300

    исправление автоматического мержа

--- blocks/editor/editor.js
+++ blocks/editor/editor.js
@@ -3,15 +3,19 @@ var Team1 = Team1 || {}
 Team1.Editor = function () {
   _.bindAll(this, "onCursorActivity")
 
-  this.cursorHtml = "<div class='CodeMirror-cursor cm_cursor'/>"
-  this.cursorsContainerEl = $(".CodeMirror-cursors")
-
-  this.codeEditor = CodeMirror.fromTextArea($("#docEditor")[0]
-    , { mode: "javascript"
-      , lineNumbers: true
+  this.codeEditor = CodeMirror.fromTextArea($("#docEditor")[0],
+    {
+      lineNumbers: true
       , matchBrackets: true
-    }
-  )
+      , foldGutter: true        //сворачивание кода
+      , mode: "javascript"
+    })
+
+  this.getThemesList()
+
+  this.changeEditorMode()
+
+  this.getDefaultEditorMode()
 
   this.codeEditor.on("cursorActivity", this.onCursorActivity)
   this.cursors = []
@@ -20,26 +24,27 @@ Team1.Editor = function () {
 
 Team1.Editor.prototype.onCursorActivity = function () {
   var cursor = this.codeEditor.getCursor()
-  var meta =  { a: "meta"
-              , document:
-                { id: Team1.documentId
-                }
-              , id: Team1.__user.id
-              , meta:cursor
-              }
+  var meta = {
+    a: "meta"
+    , document: {
+      id: Team1.documentId
+    }
+    , id: Team1.__user.id
+    , meta: cursor
+  }
   Team1.send(JSON.stringify(meta))
 }
 
 Team1.Editor.prototype.addCursor = function (cursorInfo) {
-  var opt = { className: this.getCursorClass(cursorInfo.id, cursorInfo.color)}
-  , to = {
-    ch: cursorInfo.position.ch + 1,
-    line: cursorInfo.position.line
-  }
+  var opt = {className: this.getCursorClass(cursorInfo.id, cursorInfo.color)}
+    , to = {
+      ch: cursorInfo.position.ch + 1,
+      line: cursorInfo.position.line
+    }
 
   var cursor = this.codeEditor.markText(cursorInfo.position, to, opt)
   if (cursor.lines.length)
-    this.cursors.push({id:cursorInfo.id, cursor: cursor})
+    this.cursors.push({id: cursorInfo.id, cursor: cursor})
   else
     this.addCursorOnLineEnd(cursorInfo)
 }
@@ -50,15 +55,15 @@ Team1.Editor.prototype.getCursorClass = function (id, color) {
 
 Team1.Editor.prototype.addCursorOnLineEnd = function (cursorInfo) {
   var opt = {
-    className: this.getCursorClassAfter(cursorInfo.id, cursorInfo.color)
-  }
-  , to = {
-    ch: cursorInfo.position.ch - 1,
-    line: cursorInfo.position.line
-  }
+      className: this.getCursorClassAfter(cursorInfo.id, cursorInfo.color)
+    }
+    , to = {
+      ch: cursorInfo.position.ch - 1,
+      line: cursorInfo.position.line
+    }
 
   var cursor = this.codeEditor.markText(to, cursorInfo.position, opt)
-  this.cursors.push({id:cursorInfo.id, cursor: cursor})
+  this.cursors.push({id: cursorInfo.id, cursor: cursor})
 }
 
 Team1.Editor.prototype.getCursorClassAfter = function (id, color) {
@@ -74,7 +79,7 @@ Team1.Editor.prototype.removeCursor = function (id) {
   for (var i = this.cursors.length - 1; i >= 0; i--) {
     if (this.cursors[i].id === id) {
       this.cursors[i].cursor.clear()
-      this.cursors.splice(i,1)
+      this.cursors.splice(i, 1)
     }
   }
 }
@@ -85,7 +90,7 @@ Team1.Editor.prototype.addSelection = function (selectionInfo) {
   }
 
   var sel = this.codeEditor.markText(selectionInfo.from, selectionInfo.to, opt)
-  this.selections.push({id:selectionInfo.id, sel: sel})
+  this.selections.push({id: selectionInfo.id, sel: sel})
 }
 
 Team1.Editor.prototype.getSelectionClass = function (id, color) {
@@ -101,7 +106,87 @@ Team1.Editor.prototype.removeSelection = function (id) {
   for (var i = this.selections.length - 1; i >= 0; i--) {
     if (this.selections[i].id === id) {
       this.selections[i].sel.clear()
-      this.selections.splice(i,1)
+      this.selections.splice(i, 1)
     }
+  }
+}
+
+Team1.Editor.prototype.getThemesList = function () {
+  var self = this
+
+  $.get("/theme", function (data) {
+    self.themesList = JSON.parse(data)
+  }).done(function () {
+    self.setThemesList()
+  })
+}
+
+Team1.Editor.prototype.setThemesList = function () {
+  var $themesList = $(".control__themelist")
+
+  this.themesList.forEach(function (theme) {
+    $themesList.append("<option>" + theme.slice(0, -4) + "</option>")
+  })
+
+  $("body").append("<style class='theme_style'>")
+
+  this.addHandlerToThemeOption()
+}
+
+Team1.Editor.prototype.addHandlerToThemeOption = function () {
+  var self = this
+    , theme
+    , $themesList = $(".control__themelist")
+
+  $themesList.on("change", function () {
+    theme = $(this).find("option:selected").text()
+
+    self.setTheme(theme)
+  })
+}
+
+Team1.Editor.prototype.setTheme = function (theme) {
+  var self = this
+
+  $.get("/theme", {name: theme})
+    .done(function (data) {
+      $(".theme_style").text(JSON.parse(data))
+      self.codeEditor.setOption("theme", theme)
+    }).fail(function () {
+      console.log("Error downloading theme")
+    })
+}
+
+Team1.Editor.prototype.changeEditorMode = function () {
+  var $header = $(".header")
+    , $roster = $(".roster")
+
+  $(".js-editor-mode-switch").on("change", function () {
+    if ($(this).is(":checked")) {
+      $header.removeClass("header--dark").addClass("header--light")
+      $roster.removeClass("roster--dark").addClass("roster--light")
+    } else {
+      $header.removeClass("header--light").addClass("header--dark")
+      $roster.removeClass("roster--light").addClass("roster--dark")
+    }
+  })
+}
+
+Team1.Editor.prototype.getDefaultEditorMode = function () {
+  var editorMode = "light" //light or dark
+
+  this.setDefaultEditorMode(editorMode);
+}
+Team1.Editor.prototype.setDefaultEditorMode = function (editorMode) {
+  var $header = $(".header")
+    , $roster = $(".roster")
+    , $switchMode = $(".js-editor-mode-switch")
+
+  $header.addClass("header--" + editorMode)
+  $roster.addClass("roster--" + editorMode)
+
+  if (editorMode == "light") {
+    // $switchMode.prop("checked", true) // don't work
+    $switchMode.click();
   }
 }
