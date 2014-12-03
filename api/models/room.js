a8bd71e55b1b9fd64601da77e5062e84ad5079a9
Evgeny Traytyak <evgen.traytyak@gmail.com>
2014-12-04 02:33:55 +0300

    Add language field in room model

--- api/models/room.js
+++ api/models/room.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose'),
 
 var RoomsSchema = new Schema(
   { name: { type: String, trim: true, required: true, unique: true }
+  , language: { type: String, default: 'javascript'}
   , created_at: { type: Date, default: Date.now()}
   , creator_id: { type: Schema.ObjectId, required: true, ref: 'User' }
   , user_ids: [{ type: Schema.ObjectId, ref: 'User'}]
